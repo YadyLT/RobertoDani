b8c8dfbd54324cb43eedb5a89eedb29816d34ac6
BerthaGr <BerthaGr@gmail.com>
2024-05-03 17:08:01 +0200

    add carrucel component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CarrucelComponent } from './component/carrucel/carrucel.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'carrucel' ,component:CarrucelComponent }
+];
