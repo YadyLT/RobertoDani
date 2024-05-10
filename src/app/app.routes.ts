263ac876ebc25af72bc1d396315e30cbe2e0d48a
BerthaGr <BerthaGr@gmail.com>
2024-05-10 17:12:40 +0200

    cambios en el navbar y en el componente carrucel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { CarrucelComponent } from './component/carrucel/carrucel.component';
 
 export const routes: Routes = [
-    {path: 'carrucel' ,component:CarrucelComponent }
+    {path: 'home' , component:CarrucelComponent }
 ];
