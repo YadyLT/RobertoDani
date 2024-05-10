925d0ca468d707cc6c3810c56445235fa81ee721
BerthaGr <BerthaGr@gmail.com>
2024-05-10 17:35:07 +0200

    cambio de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
-import { CarrucelComponent } from './component/carrucel/carrucel.component';
+import { CarrucelComponent } from './componentes/carrucel/carrucel.component';
+
 
 export const routes: Routes = [
     {path: 'home' , component:CarrucelComponent }
